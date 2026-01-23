4e77dfc2f63aca61d8ce01322c9ef3d4429d10b3
Syahrul G Ramdhani <74161465+syagura@users.noreply.github.com>
2026-01-23 16:37:47 +0700

    Update api.js
    update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:8000";
-const WS_BASE_URL = "ws://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const WS_BASE_URL = import.meta.env.VITE_WS_BASE_URL;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
